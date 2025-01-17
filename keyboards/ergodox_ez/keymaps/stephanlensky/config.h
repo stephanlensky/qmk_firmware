0a5e8f1a83caf3c05265f9fd106c5f7db3308558
Stephan Lensky <public@slensky.com>
2025-01-17 13:47:53 -0500

    Add stephanlensky keymap for Ergodox Ez

---
+++ keyboards/ergodox_ez/keymaps/stephanlensky/config.h
@@ -0,0 +1,18 @@
+/*
+  Set any config.h overrides for your specific keymap here.
+  See config.h options at https://docs.qmk.fm/#/config_options?id=the-configh-file
+*/
+// https://precondition.github.io/home-row-mods#what-are-home-row-mods
+#define ORYX_CONFIGURATOR
+#undef DEBOUNCE
+#define DEBOUNCE 50
+#define COMBO_TERM 100
+#define TAPPING_TERM 230
+#define QUICK_TAP_TERM 0
+#define PERMISSIVE_HOLD
+
+#define USB_SUSPEND_WAKEUP_DELAY 0
+#define FIRMWARE_VERSION u8"KYDDl/Z7j6J"
+#define RAW_USAGE_PAGE 0xFF60
+#define RAW_USAGE_ID 0x61
+#define LAYER_STATE_8BIT
